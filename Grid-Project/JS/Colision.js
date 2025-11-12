7cf006d583ca8a56c0cfd409da00070b36491949
Samuel Soares 2 kk <samuelsoaresdossantosinfinity@gmail.com>
2025-11-12 09:05:38 -0300

    Delete Grid-Project directory

--- Grid-Project/JS/Colision.js
+++
@@ -1,8 +0,0 @@
-function colisionGrid(el1, el2) {
-    const row1 = parseInt(el1.style.gridRow);
-    const col1 = parseInt(el1.style.gridColumn);
-    const row2 = parseInt(el2.style.gridRow);
-    const col2 = parseInt(el2.style.gridColumn);
-
-    return row1 === row2 && col1 === col2;
-}
