522b9c12392a0a611b9060f5d24efede1a1955c8
Samuel Soares 2 kk <samuelsoaresdossantosinfinity@gmail.com>
2025-11-12 08:33:28 -0300

    Delete Clicker directory

--- Clicker/código/LocalStorage.js
+++
@@ -1,5 +0,0 @@
-const nome = document.getElementById("input-nome").value.trim();
-const recadinho = document.getElementById("input-recado").value.trim();
-
-localStorage.setItem("nome", nome);
-localStorage.setItem("recadinho", recadinho);
